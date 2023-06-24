b68b19778c68a4b98f70af1b8e5a6d1ea9631187
kibrom-22 <110070778+kibrom-22@users.noreply.github.com>
2023-06-24 19:02:03 -0400

    Add files via upload

---
+++ Full_Stack_Demo/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "EMPLOYEES")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "email")
+	private String email;
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core'
+import { FormsModule } from '@angular/forms';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
