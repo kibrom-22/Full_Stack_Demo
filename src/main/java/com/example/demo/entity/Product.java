333e85f809792a938e836f33e0282f16eaa2c6f0
= <=>
2023-07-10 10:21:39 -0400

    Product entity and repository added

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,5 +1,25 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	@Column(name = "product_name")
+	private String productName;
+	@Column(name = "qty")
+	private int qty;
+	@Column(name = "price")
+	private int price;
 
 }
